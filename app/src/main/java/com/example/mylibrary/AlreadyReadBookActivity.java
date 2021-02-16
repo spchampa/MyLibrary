5499c5eb6253a03e6654f5b430e6f337feb7ab3e
spchampa <st.champa@gmail.com>
2021-02-15 18:53:34 -0700

    On back pressed navigate to main activity from already read books activity and clear the backstack.

--- app/src/main/java/com/example/mylibrary/AlreadyReadBookActivity.java
+++ app/src/main/java/com/example/mylibrary/AlreadyReadBookActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AlreadyReadBookActivity extends AppCompatActivity {
@@ -21,5 +22,13 @@ public class AlreadyReadBookActivity extends AppCompatActivity {
 
         adapter.setBooks(Utils.getInstance().getAlreadyReadBooks());
 
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed(); // some functionality didnt work. To fix create an intent and navigate the user back to main activity.
+        Intent intent = new Intent(this, MainActivity.class); //this is the context and main activity is the destination.
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK); // clears the backstack then this navigation happens.
+        startActivity(intent);
     }
 }
