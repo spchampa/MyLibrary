08a7c6f648f8d464f1575ee55564d5827367efa3
spchampa <st.champa@gmail.com>
2021-02-16 18:22:59 -0700

    enabled action bar home back button in all books activity

--- app/src/main/java/com/example/mylibrary/AllBooksActivity.java
+++ app/src/main/java/com/example/mylibrary/AllBooksActivity.java
@@ -1,11 +1,13 @@
 package com.example.mylibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -20,7 +22,7 @@ public class AllBooksActivity extends AppCompatActivity {
         setContentView(R.layout.activity_all_books);
 
 //        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         adapter = new BookRecViewAdapter(this, "allBooks");
         booksRecyclerView = findViewById(R.id.booksRecyclerView);
@@ -36,4 +38,17 @@ public class AllBooksActivity extends AppCompatActivity {
 //        super.finish();
 //        overridePendingTransition(R.anim.slide_out, R.anim.slide_in);
 //    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
